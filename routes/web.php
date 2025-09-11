a83a0c139514694bbc8ed7f6bef103d84afe3e88
Sergio Alejandro Amaya <zsergioalejandro27@gmail.com>
2025-09-11 10:58:24 -0500

    Rutas Basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('products', function () {
+    return "LISTADO DE PRODUCTOS";
+});
+
+Route::get('products/create', function () {
+    return "FORMULARIO DE CREACIÓN DE PRODUCTOS";
+});
+
+Route::get('products/{id}/{category?}', function ($id, $category = null) {
+    if ($category == null) {
+        return "Deatlle de cada producto: " . $id;
+    } else {
+        return "Deatlle de cada producto: " . $id . " de la categoria: " . $category;
+    }
+});
