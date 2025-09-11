85b7a5b8401723777025f3ed6c7708ec274fda42
Sergio Alejandro Amaya <zsergioalejandro27@gmail.com>
2025-09-11 11:57:34 -0500

    Vistas basicas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,20 +8,16 @@ class ProductController extends Controller
 {
     function index()
     {
-        return "LISTADO DE PRODUCTOS";
+        return view('products.index');
     }
 
     function create()
     {
-        return "FORMULARIO DE CREACIÓN DE PRODUCTOS";
+        return view('products.create');
     }
 
     function show($id, $category = null)
     {
-        if ($category == null) {
-            return "Deatlle de cada producto: " . $id;
-        } else {
-            return "Deatlle de cada producto: " . $id . " de la categoria: " . $category;
-        }
+        return view('products.show');
     }
 }
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>FORM TO CREATE A PRODUCT</h1>
+
+    <form action="" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name">
+        <br>
+
+        <label for="description">Description:</label>
+        <textarea name="description" cols="30" rows="10"></textarea>
+        <br>
+
+        <label for="price">Price</label>
+        <input type="number" name="price">
+        <br>
+
+        <label for="image">Image:</label>
+        <input type="file" name="image">
+        <br>
+
+        <label for="brand">Brand</label>
+        <input type="text" name="brand">
+
+
+    </form>
+</body>
+</html>
