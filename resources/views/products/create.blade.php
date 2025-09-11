9e5a2e2e110d73f482250a54b4650d8ebba14289
Sergio Alejandro Amaya <zsergioalejandro27@gmail.com>
2025-09-11 12:42:35 -0500

    Layouts Basico

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>FORM TO CREATE A PRODUCT</h1>
 
     <form action="" method="post">
@@ -31,5 +25,4 @@
 
 
     </form>
-</body>
-</html>
+@endsection
