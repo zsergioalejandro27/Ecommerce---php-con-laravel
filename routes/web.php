0120b665fef6be850aef857d967d83935b596e33
Sergio Alejandro Amaya <zsergioalejandro27@gmail.com>
2025-09-11 11:37:39 -0500

    Grupo de rutas basico

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('products', [ProductController::class, 'index']);
+Route::prefix('products')->controller(ProductController::class)->group(function () {
+    Route::get('/', 'index');
 
-Route::get('products/create', [ProductController::class, 'create']);
+    Route::get('/create',  'create');
 
-Route::get('products/{id}/{category?}', [ProductController::class, 'show']);
+    Route::get('/{id}/{category?}', 'show');
+});
